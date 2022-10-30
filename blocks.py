97486252dc821e1a85f9ed25dad476ac14b33c07
Sidharth Nair <sidnair@utexas.edu>
2022-10-30 01:14:59 -0500

    SRGAN model defined based on paper, have not done testing yet

---
+++ blocks.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+
+# Implementation of SRGAN as described in https://arxiv.org/pdf/1609.04802.pdf
+# The following classes define the building blocks for the generator and discriminator networks
+
+
+class ConvBlock(nn.Module):
+    # Convolutional layer with optional batch normalization and/or activation function
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, batch_norm=False, activation=None):
+        super().__init__()
+
+        layers = []
+        layers.append(nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=(kernel_size // 2)
+        ))
+
+        if batch_norm:
+            layers.append(nn.BatchNorm2d(num_features=out_channels))
+
+        if activation is not None:
+            match activation:
+                case 'leakyrelu':
+                    layers.append(nn.LeakyReLU(0.2, inplace=True))
+                case 'prelu':
+                    layers.append(nn.PReLU())
+                case 'tanh':
+                    layers.append(nn.Tanh())
+                case other:
+                    raise RuntimeError(
+                        'Unsupported activation function:', activation)
+
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class ResidualBlock(nn.Module):
+    # Two convolutional layers with a skip connection
+    def __init__(self, num_channels, kernel_size, stride):
+        super().__init__()
+        self.conv_block1 = ConvBlock(
+            in_channels=num_channels,
+            out_channels=num_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            batch_norm=True,
+            activation='prelu'
+        )
+        self.conv_block2 = ConvBlock(
+            in_channels=num_channels,
+            out_channels=num_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            batch_norm=True,
+            activation=None
+        )
+
+    def forward(self, x):
+        y = self.conv_block1(x)
+        y = self.conv_block2(y)
+        return x + y # Skip connection
+
+
+class UpsampleBlock(nn.Module):
+    # Uses convolution reshaping to perform upsampling
+    def __init__(self, in_channels, kernel_size, upscale_factor):
+        super().__init__()
+        self.conv = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=(in_channels * upscale_factor ** 2),
+            kernel_size=kernel_size,
+            stride=1,
+            padding=(kernel_size // 2)
+        )
+        self.ps = nn.PixelShuffle(upscale_factor=upscale_factor)
+        self.prelu = nn.PReLU()
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.ps(x)
+        x = self.prelu(x)
+        return x
---
+++ models.py
@@ -0,0 +1,102 @@
+from math import log2
+import torch
+import torch.nn as nn
+
+from blocks import ConvBlock, ResidualBlock, UpsampleBlock
+
+# Implementation of SRGAN as described in https://arxiv.org/pdf/1609.04802.pdf
+
+
+class Generator(nn.Module):
+    def __init__(self, in_channels=3, num_residual_blocks=16, scaling_factor=4):
+        super().__init__()
+
+        # Convolution (k9n64s1) followed by PReLU
+        self.conv_block1 = ConvBlock(
+            in_channels=in_channels,
+            out_channels=64,
+            kernel_size=9,
+            stride=1,
+            batch_norm=False,
+            activation='prelu'
+        )
+
+        # B residual blocks (k3n64s1)
+        residual_block_list = [ResidualBlock(num_channels=64, kernel_size=3, stride=1)
+                               for i in range(num_residual_blocks)]
+        self.residual_blocks = nn.Sequential(*residual_block_list)
+
+        # Convolution (k3n64s1) followed by batch normalization
+        self.conv_block2 = ConvBlock(
+            in_channels=64,
+            out_channels=64,
+            kernel_size=3,
+            stride=1,
+            batch_norm=True,
+            activation=None
+        )
+
+        # Upsampling layers (k3n256s1)
+        upsample_block_list = [UpsampleBlock(in_channels=64, kernel_size=3, upscale_factor=2)
+                               for i in range(int(log2(scaling_factor)))]
+        self.upsample_blocks = nn.Sequential(*upsample_block_list)
+
+        # Final convolution (k9n3s1)
+        self.conv_block3 = ConvBlock(
+            in_channels=64,
+            out_channels=in_channels,
+            kernel_size=9,
+            stride=1,
+            batch_norm=False,
+            activation='tanh'
+        )
+
+    def forward(self, input):
+        initial = self.conv_block1(input)
+        output = self.residual_blocks(initial)
+        output = self.conv_block2(output)
+        output = initial + output  # Skip connection
+        output = self.upsample_blocks(output)
+        output = self.conv_block3(output)
+        return output
+
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels=3):
+        super().__init__()
+
+        # 8 convolutional blocks
+        conv_block_list = []
+
+        # Number of output channels for each convolutional block
+        out_channels = [64, 64, 128, 128, 256, 256, 512, 512]
+
+        for i in range(len(out_channels)):
+            # First block does not have batch normalization
+            # Stride alternates between 1 and 2 for each block
+            conv_block_list.append(ConvBlock(
+                in_channels=in_channels,
+                out_channels=out_channels[i],
+                kernel_size=3,
+                stride=(1 if i % 2 == 0 else 2),
+                batch_norm=(i != 0),
+                activation='leakyrelu'
+            ))
+            in_channels = out_channels[i]
+        self.conv_blocks = nn.Sequential(*conv_block_list)
+
+        # Classifies whether the input is LR or HR
+        # Two dense layers with a final sigmoid activation for classification
+        self.classifier_block = nn.Sequential(
+            nn.AdaptiveAvgPool2d(output_size=(6, 6)),
+            nn.Flatten(),
+            nn.Linear(512 * 6 * 6, 1024),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(1024, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.conv_blocks(x)
+        x = self.classifier_block(x)
+        return x
